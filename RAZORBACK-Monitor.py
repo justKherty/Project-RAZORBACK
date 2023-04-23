8442df262140bcfa1b239f40efb8b0fe81cc6c65
Kherty <defendsaltair@gmail.com>
2023-04-23 21:10:30 +0200

    Update RAZORBACK-Monitor.py

--- RAZORBACK-Monitor.py
+++ RAZORBACK-Monitor.py
@@ -4,30 +4,27 @@ import win32api
 import win32gui
 import win32process
 import pythoncom
-import pyHook
+import pyWinhook as pyHook
 
-# Set the name of the program to monitor
-program_name = "PROGRAM.exe"
+def on_key_press(event):
+    print(event.Key)
+    return True
 
-# Set the list of monitored keys
+program_name = "Freelancer.exe"
 monitored_keys = ["Oem_Period", "Oem_Comma", "Space", "Return"]
-
-# Initialise the dictionary of pressed keys
 pressed_keys = {}
 
-# Set up the hook manager
 hook_manager = pyHook.HookManager()
-hook_manager.KeyDown = lambda event: on_key_press(event)
+#hook_manager.KeyDown = lambda event: on_key_press(event)
+hook_manager.KeyDown = on_key_press
 hook_manager.HookKeyboard()
 
-# Set up the process handle
 pid = None
 hwnd = win32gui.GetForegroundWindow()
 if win32gui.IsWindowVisible(hwnd):
     _, pid = win32process.GetWindowThreadProcessId(hwnd)
 process_handle = psutil.Process(pid) if pid else None
 
-# Log the network connections made by the process
 def log_network_connections():
     if process_handle:
         with open(f"{program_name}_network_connections.txt", "w") as f:
@@ -37,11 +34,9 @@ def log_network_connections():
                 f.write(f"Family: {conn.family}, Type: {conn.type}, Local Address: {conn.laddr}, Remote Address: {conn.raddr}, Status: {conn.status}\n")
             f.write("\n")
 
-# Start the message loop to capture keystrokes
 while True:
     pythoncom.PumpWaitingMessages()
 
-    # Check if the current process has changed
     new_hwnd = win32gui.GetForegroundWindow()
     if hwnd != new_hwnd:
         hwnd = new_hwnd
@@ -50,7 +45,6 @@ while True:
         process_handle = psutil.Process(pid) if pid else None
         log_network_connections()
 
-# Handler for key presses
 def on_key_press(event):
     key = event.Key
     if key in monitored_keys:
@@ -59,7 +53,6 @@ def on_key_press(event):
             log_key_press(key)
     return True
 
-# Log the pressed key
 def log_key_press(key):
     if process_handle:
         with open(f"{program_name}_keylog.txt", "a") as f:
